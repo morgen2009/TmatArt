e8cac45dc527534383aaed01f30370ec05d1c2d4
morgen2009 <morgen2009@gmail.com>
2012-12-24 13:37:01 +0100

    implemented Scattering.Field.{RegionField, Superposition}

--- Tmatrix/Scattering/Field/RegionField.cs
+++ Tmatrix/Scattering/Field/RegionField.cs
@@ -1,13 +1,51 @@
 using System;
 using TmatArt.Geometry.Region;
+using TmatArt.Geometry;
 using TmatArt.Numeric.Mathematics;
 
 namespace TmatArt.Scattering.Field
 {
-	public abstract class RegionField : Field
+	/// <summary>
+	/// Compute the electromagnetic field inside the given region (near field).
+	/// The field outside this regions is assumed to be zero
+	/// </summary>
+	public class RegionField : Field
 	{
-		public Field field;
-		public IRegion region;
+		public readonly Field field;
+		public readonly IRegion region;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TmatArt.Scattering.Field.RegionField"/> class.
+		/// </summary>
+		/// <param name="field">Electromagnetic field.</param>
+		/// <param name="region">Region where the field is defined.</param>
+		public RegionField (Field field, IRegion region)
+		{
+			this.field = field;
+			this.region = region;
+		}
+
+		/// <see cref="TmatArt.Scattering.Field.NearE"/>
+		public override Vector3c NearE (Vector3d r)
+		{
+			if (region.inside(r)) {
+				return field.NearE(r);
+			} else {
+				return new Vector3c();
+			}
+		}
+
+		/// <see cref="TmatArt.Scattering.Field.FarE"/>
+		public override Vector3c FarE (Euler e)
+		{
+			return new Vector3c();
+		}
+
+		/// <see cref="TmatArt.Scattering.Operation<T>"/>
+		public override T Operation<T> ()
+		{
+			return field.Operation<T>();
+		}
 	}
 }
 
--- Tmatrix/Scattering/Field/Superposition.cs
+++ Tmatrix/Scattering/Field/Superposition.cs
@@ -1,13 +1,82 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 using TmatArt.Geometry;
 using TmatArt.Numeric.Mathematics;
 
 namespace TmatArt.Scattering.Field
 {
-	public abstract class Superposition : Field
+	/// <summary>
+	/// Compute superposition of fields (with weights)
+	/// </summary>
+	public class Superposition : Field
 	{
-		public Field   [] fields;
-		public Complex [] weights;
+		public List<KeyValuePair<Field, Complex>> fields;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TmatArt.Scattering.Field.Superposition"/> class.
+		/// </summary>
+		/// <param name="fields">Array of fields.</param>
+		public Superposition (IEnumerable<Field> fields)
+		{
+			this.fields = new List<KeyValuePair<Field, Complex>>();
+
+			foreach (Field f in fields) {
+				this.AddField(f);
+			}
+		}
+
+		/// <summary>
+		/// Adds the field into superposition
+		/// </summary>
+		/// <param name="field">Field.</param>
+		/// <param name="weight">Weight.</param>
+		public void AddField(Field field, Complex weight)
+		{
+			this.fields.Add(new KeyValuePair<Field, Complex>(field, weight));
+		}
+		
+		/// <summary>
+		/// Adds the field into superposition with default weight (1)
+		/// </summary>
+		/// <param name="field">Field.</param>
+		public void AddField(Field field)
+		{
+			this.AddField(field, Complex.ONE);
+		}
+		
+		/// <see cref="TmatArt.Scattering.Field.NearE"/>
+		public override Vector3c NearE (Vector3d r)
+		{
+			return fields.Sum( f => f.NearE(r) );
+		}
+		
+		/// <see cref="TmatArt.Scattering.Field.FarE"/>
+		public override Vector3c FarE (Euler e)
+		{
+			return fields.Sum( f => f.FarE(e) );
+		}
+		
+		/// <see cref="TmatArt.Scattering.Operation<T>"/>
+		public override T Operation<T> ()
+		{
+			// TODO
+			return default(T);
+		}
+	}
+
+	public static class SuperpositionExtension
+	{
+		public static Vector3c Sum(this IEnumerable<KeyValuePair<Field, Complex>> fields, Func<Field, Vector3c> func)
+		{
+			Vector3c res = new Vector3c();
+			
+			foreach (var item in fields) {
+				res = res + func(item.Key) * item.Value;
+			}
+			
+			return res;
+		}
 	}
 }
 
